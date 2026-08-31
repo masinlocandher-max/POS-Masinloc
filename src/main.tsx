b777420f967bdae6ebe919983b1f991f2617073d
masinlocandher-max <masinlocandher@gmail.com>
2026-08-31 08:56:58 +0800

    Use Supabase merchant app for business routes

--- src/main.tsx
+++ src/main.tsx
@@ -1,17 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
-import MerchantMvp from './MerchantMvp'
 import StagingGate from './StagingGate'
 import './styles.css'
-
-const normalizedPath = window.location.pathname.replace(/\/+$/, '')
-const isMerchantMvp = normalizedPath.endsWith('/business') || normalizedPath.endsWith('/merchant')
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <StagingGate>
-      {isMerchantMvp ? <MerchantMvp /> : <App />}
+      <App />
     </StagingGate>
   </React.StrictMode>,
 )

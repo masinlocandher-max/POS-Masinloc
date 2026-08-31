6cd2444f0ece8a9ad3bfbf5f92b62d34f0f08c67
masinlocandher-max <masinlocandher@gmail.com>
2026-08-31 08:49:00 +0800

    Expose merchant MVP route

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,19 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
+import MerchantMvp from './MerchantMvp'
 import RegistrationGate from './RegistrationGate'
 import StagingGate from './StagingGate'
 import './styles.css'
+
+const normalizedPath = window.location.pathname.replace(/\/+$/, '')
+const isMerchantMvp = normalizedPath.endsWith('/business') || normalizedPath.endsWith('/merchant')
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <StagingGate>
       <RegistrationGate>
-        <App />
+        {isMerchantMvp ? <MerchantMvp /> : <App />}
       </RegistrationGate>
     </StagingGate>
   </React.StrictMode>,
